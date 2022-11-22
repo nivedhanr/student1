aad548019c0e460c4913472eddf79f822086f2a7
nivedhanr <nivedharenahan@gmail.com>
2022-11-22 09:50:11 +0530

    using arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,92 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+class Student {
 
+    String StudentName;
+    int AdNo;
+    int RollNo;
+    String CollegeName;
+
+    public Student(String studentName, int adNo, int rollNo, String collegeName) {
+        this.StudentName = studentName;
+        this.AdNo = adNo;
+        this.RollNo = rollNo;
+        this.CollegeName = collegeName;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-            int c,i=2;
-            Scanner sc=new Scanner(System.in);
-            String[] name=new String[2];
-            String[] roll_no=new String[2];
-            String[] ad_num=new String[2];
-            String[] college=new String[2];
-        do {
-            System.out.println("1.Add Students\n2.List all the student\n3.Search a student with admission number\n4.Delete a student with admission number\n5.Exit");
-            System.out.println("Enter your choice:");
-            c = sc.nextInt();
-            switch (c) {
-                case 1:
-                    for (int a = 0; a < 2; a++){
-                        System.out.println("Student Details");
-                        System.out.println("Student name");
-                        name[a] = sc.next();
-                        System.out.println("Roll number");
-                        roll_no[a] = sc.next();
-                        System.out.println("Admission number");
-                        ad_num[a] = sc.next();
-                        System.out.println("College");
-                        college[a] = sc.next();
-                    }
-                    break;
-                case 2:
-                    System.out.println("Student Details");
-                    for(int a=0;a<2;a++) {
-                        System.out.println("Student name"+name[a]);
-                        System.out.println("Roll number"+roll_no[a]);
-                        System.out.println("Admission number"+ad_num[a]);
-                        System.out.println("College"+college[a]);
-                        System.out.println("");
-                    }
-                    break;
-                case 3:
-                    System.out.println("Enter the Admission number:");
-                    int s=sc.nextInt();
-                    boolean f=false;
-                    for(int a=0;a<2;a++){
-                        if(ad_num[a] == s){
-                            f=true;
-                        }
-                    }
-                    if(f==true){
-                        for(int a=0;a<2;a++){
-                            System.out.println("Student admission no"+ad_num[a]);
-                            System.out.println("Student name"+name[a]);
-                            System.out.println("Student roll number"+roll_no[a]);
-                            System.out.println("College"+college[a]);
-                            System.out.println("");
+        ArrayList<Student> Studarr = new ArrayList<Student>();
+        while (true) {
+            System.out.println("Select an option \n1.Add Student\n2.View Student\n3.Search Student\n4.Delete Student\n5.Exit");
+            Scanner xy = new Scanner(System.in);
+            int option = xy.nextInt();
+            if (option == 5) {
+                System.out.println("You are exiting from the program");
+                System.exit(0);
+            } else {
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter Students details");
+                        Scanner XY = new Scanner(System.in);
+                        System.out.println("Enter name");
+                        String ab = XY.next();
+                        System.out.println("Enter AdNo");
+                        int cd = XY.nextInt();
+                        System.out.println("Enter RollNo");
+                        int ef = XY.nextInt();
+                        System.out.println("Enter College");
+                        String gh = XY.next();
+                        Student S = new Student(ab, cd, ef, gh);
+                        Studarr.add(S);
+                        break;
+                    case 2 :
+                        System.out.println("Employee Details Showing");
+                        for (Student std : Studarr) {
+                            System.out.println("Print Student Details " );
+                            System.out.println(std.StudentName);
+                            System.out.println(std.AdNo);
+                            System.out.println(std.RollNo);
+                            System.out.println(std.CollegeName);
 
                         }
-                    }
-                    else{
-                        System.out.println("Data is not found");
-                    }
-                    break;
-                case 4:
-                    System.out.println("Enter the Admission number:");
-                    int g=sc.nextInt();
-                    int count=0;
-                    boolean f1=false;
-                    for(int a=0;a<2;a++){
-                        if(ad_num[a] == g){
-                            f1=true;
+                        break;
+                    case 3 :
+                        System.out.println("Enter Add No");
+                        Scanner sc = new Scanner(System.in);
+                        int addno1 = sc.nextInt();
+                        for (Student S1 : Studarr) {
+                            if (addno1 == S1.AdNo){
+                                System.out.println("Details of student with ad no " + addno1 + "is ");
+                                System.out.println(S1.StudentName);
+                                System.out.println(S1.AdNo);
+                                System.out.println(S1.RollNo);
+                                System.out.println(S1.CollegeName);
+                                break;
+                            }
+                            else {
+                                System.out.println("Student not found");
+                            }
                         }
-                        count++;
-                    }
-                    if(f1==true){
-                        for(int a=count;a<count;a++){
-                            ad_num[a]=ad_num[a+1];
-                            name[a]=name[a+1];
-                            roll_no[a]=roll_no[a+1];
-                            college[a]=college[a+1];
+                        break;
+                    case 4 :
+                        System.out.println("Enter Student Ad no");
+                        Scanner xz = new Scanner(System.in);
+                        int addno2 = xz.nextInt();
+                        for (Student S1 : Studarr){
+                            if (addno2 == S1.AdNo){
+                                Studarr.remove(S1);
+                                System.out.println("Student deleted is:");
+                                System.out.println(S1.StudentName);
+                                System.out.println(S1.AdNo);
+                                System.out.println(S1.RollNo);
+                                System.out.println(S1.CollegeName);
+                                break;
+                            }
                         }
-                    }
-                    for(int a=0;a<1;a++){
-                        System.out.println("Student admission no"+ad_num[a]);
-                        System.out.println("Student name"+name[a]);
-                        System.out.println("Student roll number"+roll_no[a]);
-                        System.out.println("College"+college[a]);
-                        System.out.println("");
-
-                    }
-                    break;
-
-
-
-                default:
-                    System.out.println("Invalid entry");
+                        break;
+                }
             }
-
-        }while (c!=5);
+        }
     }
 }
